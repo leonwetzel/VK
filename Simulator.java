213f4c32256357a3ad04684a80758467b6428bf5
Leon Wetzel <leonwetzel@gmail.com>
2015-01-15 13:07:33 +0100

    Nieuwe data m.b.t. VK

--- Simulator.java
+++ Simulator.java
@@ -35,7 +35,7 @@ public class Simulator
     /**
      * Construct a simulation field with default size.
      */
-    public static void main(String[] args)
+    public Simulator()
     {
         this(DEFAULT_DEPTH, DEFAULT_WIDTH);
     }
