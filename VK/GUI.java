f6b8876f715f5021d9fe197b7f16a30c8f698fcd
PaulKoning <paulkoning@live.nl>
2015-02-02 09:01:41 +0100

    Ik dacht dat dit al eerder gesynct was.

---
+++ VK/GUI.java
@@ -0,0 +1,21 @@
+import javax.swing.JTextField;
+
+public class GUI{
+
+    //JTextFields van rabbit
+    JTextField mAgeField2;
+    JTextField aNakField2;
+    JTextField vLefField2;
+    
+    //JTextFields van penguin
+    JTextField mAgeField3;
+    JTextField aNakField3;
+    JTextField vLefField3;
+    
+    //JTextFields van fox
+    JTextField mAgeField;
+    JTextField aNakField;
+    JTextField vLefField;
+    
+    
+}
